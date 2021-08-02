4cb41b1fd4ff807591c683c80cca125f8c0e9d32
vg3r <39884948+vg3r@users.noreply.github.com>
2021-08-01 20:34:55 -0400

    Added Bash Script

--- ipa.py
+++ ipa.py
@@ -7,7 +7,7 @@ from sys import platform
 
 if platform == "win32":
     ints = subprocess.check_output(["ipconfig","/all"])
-    ints = codecs.decode(ints) 
+    ints = codecs.decode(ints)
 else:
     ints = subprocess.check_output(["ifconfig", "-a"])
 
@@ -15,6 +15,6 @@ for i in ints.split('\n'):
     if re.match(r'^\w[^\.]+?:', i):
         a = re.match('^\w[^\.]+?:', i)
         print(a.group())
-    elif re.match('(.*(?:inet|IPv[46]\s).*)', i):
+    elif re.match('(.*(?:inet|IPv[46][^\%]+))', i):
         b = re.match(r'(.*(?:inet|IPv[46]|.*.\ :).*?\s.*?)\s', i)
         print(re.sub('\t', '  ', b.group()))
